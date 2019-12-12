dc6cde8155c14a4d81d12d228216dc96bc5f90d5
Mayank Solan <mayanksolan@gmail.com>
2019-12-13 02:19:44 +0530

    menu set

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 import liquidity_logo from "../assets/liquidity_logo.png";
 import "../stylesheets/Menu.css";
+import MenuItem from "./MenuItem";
 
 export default class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      navArr: ["Deal Room", "Broker-Dealer Registry", "Issuance Statistics"]
+      navArr: ["Deal Room", "Broker-Dealer Registry", "Issuance Statistics"],
+      selectedItem: null
     };
   }
 
+  clickHandler = idx => {
+    this.setState({ selectedItem: idx });
+  };
+
   render() {
+    //let btn_class = this.state.black ? "blackButton" : "whiteButton";
     const email = "support@digital.com";
+
     return (
       <div className="container">
         <img src={liquidity_logo} alt="liquidity logo" />
-        <div>
-          {this.state.navArr.map(item => (
-            <div>{item}</div>
-          ))}
+        <div className="menu-list">
+          {this.state.navArr.map((item, idx) => {
+            var is_selected = this.state.selectedItem === idx;
+            return (
+              <MenuItem
+                key={item.idx}
+                name={item}
+                onClick={() => this.clickHandler(idx)}
+                isSelected={is_selected}
+              />
+            );
+          })}
         </div>
-        <div>
+        <div className="contact-us">
           Contact us if you have any questions:{" "}
           <a href={email}>support@digital.com</a>
         </div>
---
+++ client/src/components/MenuItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class MenuItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover_flag: false
+    };
+  }
+
+  hoverEvent = () => {
+    this.setState({ hover_flag: !this.state.hover_flag });
+  };
+
+  render() {
+    var liStyle = {
+      background: "#ffffff",
+      borderLeft: "5px solid white"
+    };
+    if (this.props.isSelected || this.state.hover_flag) {
+      liStyle["background"] = "#FAFAFB";
+      liStyle["borderLeft"] = "5px solid #aab8e6";
+      liStyle["cursor"] = "pointer";
+    }
+    return (
+      <div
+        className="menu-item"
+        onClick={this.props.onClick}
+        onMouseEnter={this.hoverEvent}
+        onMouseLeave={this.hoverEvent}
+        style={liStyle}
+      >
+        {this.props.name}
+      </div>
+    );
+  }
+}
